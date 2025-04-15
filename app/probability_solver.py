dd00ff93439aa9fbe7493bbcb0ed037b619ce972
NKTKLN <nktkln@nktkln.com>
2025-04-15 21:19:33 +0300

    Fix bug when there was a limitation in the maximum number of customers

--- app/probability_solver.py
+++ app/probability_solver.py
@@ -62,9 +62,20 @@ class ProbabilitySolver:
             logger.debug(f"Обработка собственного значения g[{index + 1}] = {g}.")
             L_matrices = self._generate_L_matrix(g)
 
-            # Вычисление значений P для матриц L
-            L_det = np.linalg.det(L_matrices[0])
-            p_values[:, index] = np.linalg.det(L_matrices[1:]) / L_det
+            sign_L0, logdet_L0 = np.linalg.slogdet(L_matrices[0])
+            if sign_L0 == 0:
+                logger.warning(f"Определитель матрицы L[0] равен 0 при g[{index + 1}] = {g}, записываем NaN.")
+                p_values[:, index] = np.nan
+                continue
+
+            for index2, l_matrix in enumerate(L_matrices[1:]):
+                sign_Li, logdet_Li = np.linalg.slogdet(l_matrix)
+                if sign_Li == 0:
+                    p_values[index2, index] = np.nan
+                    continue
+
+                p_values[index2, index] = sign_Li * sign_L0 * np.exp(logdet_Li - logdet_L0)
+
             logger.debug(f"Значения P для g[{index + 1}] = {g} вычислены.")
 
         logger.info("Вычисление значений P завершено.")
@@ -73,43 +84,56 @@ class ProbabilitySolver:
     def _xsi_matrix_generator(
         self,
         p_values_matrix: NDArray[np.float64]
-    ) -> Tuple[NDArray[np.float64], float]:
+    ) -> Tuple[NDArray[np.float64], Tuple[float, float]]:
         """
         Генерирует базовую матрицу xsi и вычисляет её определитель.
 
         :param p_values_matrix: матрица значений P
         :return: Кортеж, содержащий:
             - xsi_matrix: базовая матрица xsi
-            - xsi_matrix_det: определитель базовой матрицы xsi
+            - (sign, logdet): знак и логарифм определителя
         """
         ones_row = np.ones(p_values_matrix.shape[1], dtype=np.float64)
         xsi_matrix = np.vstack([ones_row, p_values_matrix])
 
-        xsi_matrix_det = np.linalg.det(xsi_matrix)
-        logger.debug(f"Определитель базовой матрицы xsi: {xsi_matrix_det}.")
-        return (xsi_matrix, xsi_matrix_det)
+        sign, logdet = np.linalg.slogdet(xsi_matrix)
+        logger.debug(f"Определитель базовой матрицы xsi: sign = {sign}, logdet = {logdet}.")
+        return xsi_matrix, (sign, logdet)
 
     def _calculate_a_p_values(
         self,
         xsi_matrix: NDArray[np.float64], 
-        xsi_matrix_det: float,
+        xsi_matrix_signature: Tuple[float, float],
         shift: int = 0
     ) -> NDArray[np.float64]:
         """
         Вычисление значений для A_P на основе матрицы значений P.
 
         :param xsi_matrix: базовя матрица xsi
-        :param xsi_matrix_det: определитель базовой матрицы xsi
+        :param xsi_matrix_signature: кортеж (sign, logdet) базовой матрицы xsi
+        :param shift: индекс строки, куда подставляется единица
         :return: массив значений A_P
         """
+        sign_base, logdet_base = xsi_matrix_signature
         a_p_values = np.zeros(self.params.max_customers, dtype=np.float64)
+
+        if sign_base == 0:
+            logger.warning("Определитель базовой матрицы xsi равен 0. Все значения A_P будут NaN.")
+            return a_p_values
+
         for index in range(self.params.max_customers):
             temp_xsi_matrix = xsi_matrix.copy()
             temp_xsi_matrix[:, index] = 0
             temp_xsi_matrix[shift, index] = 1
-            calc = np.linalg.det(temp_xsi_matrix) / xsi_matrix_det
-            a_p_values[index] = calc
-            logger.debug(f"Вычислено значение A_P[{index}] = {calc}.")
+
+            sign_temp, logdet_temp = np.linalg.slogdet(temp_xsi_matrix)
+            if sign_temp == 0:
+                a_p_values[index] = np.nan
+                logger.warning(f"Определитель temp_xsi_matrix для столбца {index} равен 0. A_P[{index}] = NaN.")
+                continue
+
+            a_p_values[index] = sign_temp * sign_base * np.exp(logdet_temp - logdet_base)
+            logger.debug(f"Вычислено значение A_P[{index}] = {a_p_values[index]}.")
 
         logger.debug("Вычисление значений A_P завершено.")
         return a_p_values
@@ -152,7 +176,7 @@ class ProbabilitySolver:
         m_matrix = np.zeros((p_matrix.shape[0], aa_matrix.shape[1], len(self.params.time_array)), dtype=np.float64)
         for i in range(p_matrix.shape[0]):
             for j in range(aa_matrix.shape[1]):
-                m_matrix[i, j, :] = (p_matrix[i, :] * aa_matrix[:, j]) @ exp_g_t
+                m_matrix[i, j, :] = np.real((p_matrix[i, :] * aa_matrix[:, j]) @ exp_g_t)
                 logger.debug(f"Вычислено значение m_matrix[{i}, {j}, :].")
 
         logger.info("Генерация матрицы M завершена.")
